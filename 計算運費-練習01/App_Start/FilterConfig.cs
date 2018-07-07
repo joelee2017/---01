e88424f2652089bf2f717975212dcb3747589e5a
joe lee <as955133@gmail.com>
2018-07-07 22:05:28 +0800

    加入專案檔案。

---
+++ 計算運費-練習01/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace 計算運費_練習01
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ 計算運費-練習01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="計算運費_練習01.MvcApplication" Language="C#" %>
