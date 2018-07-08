db6c2c061c5d7ebd909f0b7d56244f44e88a3801
joe lee <as955133@gmail.com>
2018-07-08 19:50:14 +0800

    Selenium 測試完成

---
+++ 計算運費-練習01/Controllers/ProductController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using 計算運費_練習01.Models;
+
+namespace 計算運費_練習01.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            var companies = new List<SelectListItem>
+            {
+                new SelectListItem{ Text="黑貓", Value="1" },
+                new SelectListItem{ Text="新竹貨運", Value="2" },
+                new SelectListItem{ Text="郵局", Value="3" }
+            };
+
+            ViewBag.Company = companies;
+            return View();
+        }
+
+        [HttpPost]
+        public  ActionResult Index(ProductModels product)
+        {
+            var companies = new List<SelectListItem>
+            {
+                new SelectListItem{ Text="黑貓", Value="1" },
+                new SelectListItem{ Text="新竹貨運", Value="2" },
+                new SelectListItem{ Text="郵局", Value="3" }
+            };
+
+            ViewBag.Company = companies;
+
+            if(!ModelState.IsValid)
+            {
+                return View(product);
+            }
+
+            double fee = 0;
+            if(product.Company == 1)
+            {
+                var weight = product.Weight;
+                if(weight > 20)
+                {
+                    fee = 500;
+                }
+                else
+                {
+                    fee = 100 + weight * 10;
+                }
+            }
+            else if(product.Company == 2)
+            {
+                var size = product.Length * product.Width * product.Height;
+                // 長 * 寬 * 高(公分) * 0.0000353
+                if(product.Length > 100|| product.Width > 100 || product.Height >100)
+                {
+                    fee = size * 0.0000353 * 100 + 500;
+                }
+                else
+                {
+                    fee = size * 0.0000353 * 1200;
+                }
+            }
+            else if(product.Company == 3)
+            {
+                var feeByWeight = 80 + product.Weight * 10;
+
+                var size = product.Length * product.Width * product.Height;
+                var feeBySize = size * 0.0000353 * 1100;
+
+                if(feeByWeight < feeBySize)
+                {
+                    fee = feeByWeight;
+                }
+                else
+                {
+                    fee = feeBySize;
+                }
+            }
+
+            ViewBag.Fee = fee;
+            return View(product);
+        }
+    }
+}
---
+++ 計算運費-練習01Tests/計算運費Steps.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Text;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using 計算運費_練習01.Models;
+
+namespace 計算運費_練習01Tests
+{
+    [Binding]
+    [Scope(Feature = "計算運費")]
+    public class 計算運費Steps
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "http://localhost:3715/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            try
+            {
+                driver.Quit();// quit does not close the window
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Given(@"計算運費頁面")]
+        public void Given計算運費頁面()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/Product/Index");
+        }
+
+        [When(@"商品規格為")]
+        public void When商品規格為(Table table)
+        {
+            var productModels = table.CreateInstance<ProductModels>();
+
+            driver.FindElement(By.Id("Name")).Clear();
+            driver.FindElement(By.Id("Name")).SendKeys(productModels.Name);
+            driver.FindElement(By.Id("Length")).Clear();
+            driver.FindElement(By.Id("Length")).SendKeys(productModels.Length.ToString());
+            driver.FindElement(By.Id("Width")).Clear();
+            driver.FindElement(By.Id("Width")).SendKeys(productModels.Width.ToString());
+            driver.FindElement(By.Id("Height")).Clear();
+            driver.FindElement(By.Id("Height")).SendKeys(productModels.Height.ToString());
+            driver.FindElement(By.Id("Weight")).Clear();
+            driver.FindElement(By.Id("Weight")).SendKeys(productModels.Weight.ToString());
+        }
+
+        [When(@"選擇(.*)")]
+        public void When選擇(string shipperName)
+        {
+            new SelectElement(driver.FindElement(By.Id("Company"))).SelectByText(shipperName);
+        }
+
+        [When(@"點選計算運費")]
+        public void When點選計算運費()
+        {
+            driver.FindElement(By.XPath("//input[@value='計算運費']")).Click();
+        }
+
+        [Then(@"運費結果應為 (.*)")]
+        public void Then運費結果應為(double fee)
+        {
+            Assert.AreEqual(fee.ToString(), driver.FindElement(By.Id("fee")).Text);
+        }
+
+    }
+}
